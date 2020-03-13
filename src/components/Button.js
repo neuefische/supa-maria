629964c24824466eaf046b4e8275106aaea89306
Kathrin92 <kathrinulmer@gmx.de>
2020-03-13 15:10:30 +0100

    Updated button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 function Button(props) {
-  return <button className="button" {...props} />;
+  return <button {...props} />;
 }
 
 export default Button;

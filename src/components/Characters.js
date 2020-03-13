56d08dc907210ef27fd97c34e1b191a191a71a1c
hauptdigital <marc@haupt.digital>
2020-03-13 17:18:08 +0100

    Add princess images from array

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,25 +3,30 @@ import Princess from "./Princess";
 import "./Characters.css";
 
 function Characters(props) {
+  const princessData = [
+    "./assets/Princess_Bubblegum.png",
+    "./assets/Lumpy_Space.png",
+    "./assets/Wildberry.png",
+    "./assets/Hot_Dog_Princess.png",
+    "./assets/Flame_Queen.png",
+    "./assets/Bee_Princess.png",
+    "./assets/Cotton_Candy_Princess.png",
+    "./assets/Princess_Cookie.png",
+    "./assets/Desert_Princess.png",
+    "./assets/BEPRINCEES.png",
+    "./assets/Jungle_Princess.png",
+    "./assets/Taoprriecess.png",
+    "./assets/Muscle_Princess_Revised.png",
+    "./assets/FrozenYogurtPrincess.png",
+    "./assets/slime.png",
+    "./assets/Peanut_princess.png"
+  ];
   return (
     <section className="characters" {...props}>
       <div className="charactersWrapper">
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
-        <Princess className="princess" />
+        {princessData.map(function(princess, index) {
+          return <Princess className="princess" imgsource={princess} />;
+        })}
       </div>
     </section>
   );
--- src/components/Princess.js
+++ src/components/Princess.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Princess.css";
 
 function Princess(props) {
-  return <div className="princess" {...props} />;
+  const imgPath = props.imgsource;
+
+  return (
+    <div className="princess" {...props}>
+      <img class="princessImage" src={imgPath} alt="princess" />
+    </div>
+  );
 }
 
 export default Princess;

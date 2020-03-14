5b8cc2bc9031d8991a2b17562dbaf36b9e945139
meloneminze <melanie@derrabus.de>
2020-03-14 19:56:33 +0100

    Fixed header

--- src/Pages/SelectPage.js
+++ src/Pages/SelectPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./SelectPage.css";
-import Header from "../components/Header";
+
 import Characters from "../components/Characters";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 function Select() {
   return (
     <>
-      <Header />
       <main>
         <h2 className="headline">Choose your princesses</h2>
         <Characters className="characters"></Characters>

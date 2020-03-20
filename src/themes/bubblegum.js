52c4aa1e944e28f52c8b78529eea8b27134f8cfe
lenkly <lenk.lydia@gmail.com>
2020-03-20 10:50:50 +0100

    Set themes

---
+++ src/themes/bubblegum.js
@@ -0,0 +1,12 @@
+import Wall from "./bubblewall.jpg";
+import Switch from "./lspicon.png";
+
+const bubblegum = {
+  colors: {},
+  images: {
+    background: Wall,
+    switchicon: Switch
+  }
+};
+
+export default bubblegum;

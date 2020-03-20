8e8bc85913080ab8ad813870ffdb192c1d246658
hauptdigital <marc@haupt.digital>
2020-03-20 10:30:07 +0100

    Add API constants

---
+++ src/api/princesses.js
@@ -0,0 +1,6 @@
+const CHARACTERS_API =
+  process.env.REACT_APP_CHARACTERS_API ||
+  "https://my-json-server.typicode.com/neuefische/supa-maria/Characters";
+const GAMES_API =
+  process.env.REACT_APP_GAMES_API ||
+  "https://my-json-server.typicode.com/neuefische/supa-maria/Games";

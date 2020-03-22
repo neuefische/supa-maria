4c9e205406b2f53aeca9be376da8ec0d0bcd2953
hauptdigital <marc@haupt.digital>
2020-03-22 13:36:19 +0100

    Add confirm button wrapper

--- src/components/ConfirmTeams.js
+++ src/components/ConfirmTeams.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Button from "./Button";
+import styled from "@emotion/styled";
+
+const ConfirmButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px 0px;
+`;
 
 function ConfirmTeams({ teams, disabled, onConfirmTeamsButtonClick }) {
   return (
-    <Button
-      disabled={disabled}
-      onClick={() => onConfirmTeamsButtonClick(teams)}
-    >
-      confirm
-    </Button>
+    <ConfirmButtonWrapper>
+      <Button
+        disabled={disabled}
+        onClick={() => onConfirmTeamsButtonClick(teams)}
+      >
+        confirm
+      </Button>
+    </ConfirmButtonWrapper>
   );
 }
 
